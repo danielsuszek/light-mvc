882051cfc7ca0f31d2a5ee2abe9a6263ad82f9b8
danielsuszek <danielsuszek@gmail.com>
2016-12-10 15:14:58 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>tytul</title>
+        <!--<link rel="stylesheet" href="css/import_all_files.css">-->
+        <link rel="stylesheet" href="http://localhost/Projects/light-mvc/public/css/import_all_files.css">
+    </head>
+    <body>
+        <?php print_r($_GET['url']); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">                    
+                        <div class="panel panel-default">
+                            <div class="jumbotron jumbotron-no-margin">
+                            <h1>Albumy Wszech Czasów</h1>
+                            <p>Najważniejsze albumy, które zmieniły historię <br>
+                                muzyki rozrywkowej.</p>
+                        </div>
+                        <h1 class="list-album-header">LISTA ALBUMÓW</h1>
+                        <div class="list-group">                            
+                            <a href="#" class="list-group-item"> 
+                                <img src="http://localhost/Projects/light-mvc/public/graphics/Jimi_Hendrix_-_Electric_Ladyland.jpg" class="picture-small">
+                                Electic Ladyland - The Jimi Hendrix Experience</a>
+                            <a href="#" class="list-group-item"> 
+                                <img src="http://localhost/Projects/light-mvc/public/graphics/220px-AreyouexpUK.jpg" class="picture-small">
+                                 Are You Experienced - The Jimi Hendrix Experience</a>
+                            <a href="#" class="list-group-item"> 
+                                <img src="http://localhost/Projects/light-mvc/public/graphics/220px-Unknownpleasures.jpg" class="picture-small">
+                                 Unknown Pleasures - Joy Division</a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+<!--        
+            <div class="left-and-right">
+                <div class="left">I'm on the left</div>
+                <div class="right">I'm on the right</div>
+            </div>
+
+
+            <button type="button" class="btn btn-primary">Primary</button>
+        </div>-->
+            
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+    
+    
+</html>
+
